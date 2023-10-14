1ce5d8b12967ba67ab163e1afe5e8d4a86ef1065
Sebastien Fauque <sbfauque@gmail.com>
2023-10-13 21:57:46 -0700

    Extended class with electric car and overrode brake method.

--- mod_05_exercise/lesson05.ts
+++ mod_05_exercise/lesson05.ts
@@ -64,13 +64,13 @@ class Car {
         return `${this.worker()} is turning ${direction}`;
     }
     // This function performs work for the other method functions
-    private worker(): string {
+    protected worker(): string {
         return this._make;
     } 
 
 }
 
-let myCar1 = new Car("Tesla", "Model Y",4)
+let myCar1 = new Car("Ford", "Model T",4)
 console.log(myCar1.color);
 
 // let myCar2 = new Car('Galaxy Motors', 'Saturn', 3, "blue"); // No validation on the door numbers happens b/c it is in the constructor
@@ -88,3 +88,39 @@ console.log(myCar1.turn('right'));
 
 // myCar1.worker() // invalid
 console.log(`Number of cars: ${Car.getNumberOfCars()}.`)
+
+
+class ElectricCar extends Car {
+    // Properties unique to ElectricCar
+    private _range: number;
+
+    // Constructor
+    constructor(make: string, model: string, range: number, doors = 2) {
+        super(make, model, doors);
+        this._range = range;
+    }
+
+    // Accessors
+    get range() {
+        return this._range;
+    }
+    set range(range) {
+        this._range = range;
+    }
+
+    // Methods
+    charge() {
+        console.log(this.worker() + " is charging.")
+    }
+
+    // Overrides the brake method of the Car class
+    brake(): string {
+        return `${this.worker()}  is braking with the regenerative braking system.`
+    }
+}
+
+let spark = new ElectricCar('Spark Motors','silver', 124, 2);
+let eCar = new ElectricCar('Electric Car Co.', 'black', 263);
+console.log(eCar.doors);         // returns the default, 2
+spark.charge();                  // returns "Spark Motors is charging"
+console.log(spark.brake());  // returns "Spark Motors is braking with the regenerative braking system"
